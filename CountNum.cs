bcf197a675c4ae78cfed03df7f987cce66d8fec4
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-09 20:14:53 +0530

    [aditya]: count total number of duplicate

---
+++ CountNum.cs
@@ -0,0 +1,23 @@
+﻿namespace CountNumber
+{
+    internal class CountNum
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            int[] num = { 1, 2, 5, 12, 43, 54, 12, 43, 2, 9, 7 };
+            for(int i=0;i<num.Length;i++) 
+            {
+                for(int j=i+1;j<num.Length;j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        Console.WriteLine("duplicate number is: " + num[i]);
+                        cnt++;
+                    }
+                }
+            }
+            Console.WriteLine("Total number of duplicate element are: " + cnt);
+        }
+    }
+}
