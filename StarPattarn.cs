b3475b8a419a74989ecf6a3c5137d319797408d8
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-10 01:10:45 +0530

    [aditya]: print start pattarn

---
+++ StarPattarn.cs
@@ -0,0 +1,17 @@
+﻿namespace StarPattarn
+{
+    internal class StarPattarn
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 8; i >= 0; i--)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
