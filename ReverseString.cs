007f48727c5563b6b3a7ec944b47acc932e71a52
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-11 02:13:59 +0530

    [aditya]: Revere Given String

---
+++ ReverseString.cs
@@ -0,0 +1,16 @@
+﻿namespace ReversString
+{
+    internal class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            String str = "aditya waghmare";
+            String revStr="";
+            for(int i=0; i<str.Length; i++)
+            {
+                str1 = str[i] +str1;
+            }
+            Console.WriteLine(str1);
+        }
+    }
+}
