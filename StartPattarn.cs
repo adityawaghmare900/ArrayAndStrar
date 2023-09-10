98116a29fa6ad6727107f63f2ef4a3b630019c64
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-11 02:25:55 +0530

    [aditya]: Print Start Pattarn

---
+++ StartPattarn.cs
@@ -0,0 +1,25 @@
+﻿namespace StarPattarn2
+{
+    internal class StartPattarn
+    {
+        static void Main(string[] args)
+        {
+            int rows = 7;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (i > 0 && i < rows - 1 && j > 0 && j < rows - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
